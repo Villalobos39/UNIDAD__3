0dbef216f02a2024469a1b3f702bbf4a8af7562a
Dulce Villalobos <dulce.villalobos17@tectijuana.edu.mx>
2018-10-16 19:59:15 -0700

    3-2
    -

---
+++ E3-2_VILLALOBOSPEREZDULCE/E3-2_VILLALOBOSPEREZDULCE/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace E3_2_VILLALOBOSPEREZDULCE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue cola = new Queue();//SE CREA EL OBJETO DE QUEUE (COLA)
+            Console.WriteLine("\n\t>>CONTENIDOS AÑADIR (Enqueue) <<\n");
+            cola.Enqueue("\tDICIEMBRE");//AGREGA ELEMENTOS A LA COLA (QUEUE) , CON EL Enqueue
+            cola.Enqueue("\tNOVIEMBRE");
+            cola.Enqueue("\tOCTUBRE");
+            cola.Enqueue("\tINVIERNO");
+            cola.Enqueue("\tOTOÑO");
+            cola.Enqueue("\tNIEVE");
+            cola.Enqueue("\tLLUVIA");
+            foreach (var descripcion in cola)
+            {
+                Console.WriteLine(descripcion); //MOSTAR LOS ELEMENTOS DE LA QUEUE
+            }
+
+            Console.Write("\n\tELEMENTOS DE LA QUEUE :" + cola.Count);
+            //COUNT CUENTA LA CANTIDAD DE ELEMNTOS
+            Console.WriteLine("\n\tELEMENTO A QUITAR (Dequeue): " + cola.Dequeue());
+            //Dequeue QUITA O ELELIMINA ELEMNTOS DE LA COLA 
+            Console.WriteLine("\n\tULTIMO ELEMENTO ACTUALMENTE : " + cola.Peek());
+            //Peek MUESTRA EL PROXIMO ELEMENTO ESTE METODO TAMIEN ES UTILIZADO EN STACK
+            Console.WriteLine("\n\tELEMENTOS TOTAL DE LA QUEUE : " + cola.Count);
+            Console.WriteLine("\n\tELEMTOS QUITADOS (Dequeue)");
+            cola.Dequeue();
+            cola.Dequeue();
+            Console.WriteLine("\n\tELEMENTOS TOTAL DE LA QUEUE : " + cola.Count);
+            cola.TrimToSize(); // ESTABLECE LA CAPACIDAD DE QUEUE 
+
+            Queue<int> Cola = new Queue<int>(); // QUEUE < TIPO DE DATO >
+            //AGREGA ELEMENTOS A LA COLA (QUEUE) CON UN TIPO DE DATO PREDETERMINADO;
+            Cola.Enqueue(3);
+            Cola.Enqueue(9);
+            Cola.TrimExcess();
+            Console.WriteLine("\n\tCONTENIDOS NUEVOS (Enqueue) Y COPIADOS:");
+            // IMPRIMO LOS ELEMENTOS COPIADOS ADEMAS DE QUE ESTE METODO TAMBIEN SE HACEN EN STACK.
+            foreach (var caracter in Cola)
+            {
+                Console.WriteLine("\t" + caracter);
+            }
+            Console.Write("\n\tSE ENCUENTRA LA PALABRA INVIRNO :  " + Cola.Contains(3));
+            //EL METODO Contains ES EL QUE REGRESA UN TRUE O FALSE SI UN ELEMENTO ESTA EN LA COLA TAMBIEN EN STACK
+            Console.WriteLine("\n\tELEMENTOS TOTAL DE LA QUEUE : " + (Cola.Count + cola.Count));
+            Console.Write("\n\t" + Cola.Equals(33));
+            Console.ReadKey();
+
+            /* EN CONCLUSION : 
+                 * LOS METODOS :   PUSH Y ENQUEUE REPRESENTAN EL AÑADIR ELEMENTOS 
+                 * LOS METODOS :   POPO Y DEQUEUE REPRESENTAN EL QUITAR Y ELIMINAR ELEMENTOS 
+                 * EN SI EL METODO PEEK TIENE LA MISMA SINTAXIS Y ES PARA MOSTRAR LOS ELEMENTOS 
+                 * Y CLEAR (),  CONTANINS (T) , CopyTo, ToArray (),Esquals (), trimExcess 
+                 * STIENEN LA MISMA FUNCION PARA COLAS(QUEUE) Y PILAS (STACK)  */
+
+        }
+    }
+}
---
+++ E3-2_VILLALOBOSPEREZDULCE/E3-2_VILLALOBOSPEREZDULCE/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("E3-2_VILLALOBOSPEREZDULCE")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("E3-2_VILLALOBOSPEREZDULCE")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("0012b25f-0327-4876-a1b7-ed2e0068b241")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
